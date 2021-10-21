5b3b9531d7b682d0d65dbcad75e415950e88e186
Murat <muratakca.mt@gmail.com>
2021-10-21 15:40:09 +0200

    client with react added, packagejson updated

--- server.js
+++ server.js
@@ -15,6 +15,15 @@ app.use(express.json());
 // send every request to router
 app.use("/api", router)
 
+
+// production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    })
+}
+
 // yayin yapiyoruz
 app.listen(process.env.PORT, () => {
     console.log(`I am listening on port ${process.env.PORT}`)
